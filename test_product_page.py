ba6573da80f1b0c28794cbeabd8152761c8a7ecb
Danis Art <nikita.efremov1412@gmail.com>
2025-01-23 16:12:58 +0300

    create test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
 """ test_product_page """
 from .pages.product_page import PageObject
 
-
+ 
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = PageObject(browser, link)
