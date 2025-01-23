d33d4f9d136cd233ee065cd5d543ce478cc6543e
Danis Art <nikita.efremov1412@gmail.com>
2025-01-23 20:37:22 +0300

    -add links

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,20 @@
 """ test_product_page """
 from .pages.product_page import PageObject
+import pytest
 
- 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
+          
+@pytest.mark.parametrize('link', links) 
+def test_guest_can_add_product_to_basket(browser, link):
     page = PageObject(browser, link)
     page.open()
     page.click_add_product()
