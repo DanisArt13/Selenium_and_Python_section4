01cffc507dda23e4ad03e3f608561d713969257d
Danis Art <nikita.efremov1412@gmail.com>
2025-01-23 16:11:16 +0300

    create test_product_page\n -add test_guest_can_add_product_to_basket

---
+++ test_product_page.py
@@ -0,0 +1,9 @@
+""" test_product_page """
+from .pages.product_page import PageObject
+
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = PageObject(browser, link)
+    page.open()
+    page.click_add_product()
