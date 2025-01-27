a3f4d952225c8d6b85912fd022df13679801d115
Danis Art <nikita.efremov1412@gmail.com>
2025-01-27 10:49:33 +0300

    add def should_not_be_success_message

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,7 +1,7 @@
 """ product_page """
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
-from .locators import ButtonLocators
+from .locators import ButtonLocators, BasketPageLocators, ProductPageLocators
 import time
 
 class PageObject(BasePage):
@@ -10,3 +10,8 @@ class PageObject(BasePage):
         link = self.browser.find_element(*ButtonLocators.BUTTON_LINK)
         link.click()
         self.solve_quiz_and_get_code()
+        
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+           "Success message is presented, but should not be"
+           
