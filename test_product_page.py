ab95fdcd1a1bfc779d1a9a0edf9370438fafe130
Danis Art <nikita.efremov1412@gmail.com>
2025-01-27 23:40:19 +0300

    add markers

--- test_product_page.py
+++ test_product_page.py
@@ -18,7 +18,7 @@ links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?pr
           # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
           ]
 
-# @pytest.mark.need_review          
+@pytest.mark.need_review          
 @pytest.mark.parametrize('link', links) 
 def test_guest_can_add_product_to_basket(browser, link):
     page = PageObject(browser, link)
@@ -52,7 +52,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
     
-# @pytest.mark.need_review
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BasketPage(browser, link)
@@ -90,7 +90,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
         
-    # @pytest.mark.need_review
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         page = PageObject(browser, link)
