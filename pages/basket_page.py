8870df56abdd5769dc84535cc527210d53f0e22e
Danis Art <nikita.efremov1412@gmail.com>
2025-01-27 22:52:04 +0300

    create basket_page.py

---
+++ pages/basket_page.py
@@ -0,0 +1,17 @@
+""" basket page """
+
+from .base_page import BasePage
+from .locators import BasketPageLocators
+
+import pytest
+
+class BasketPage(BasePage):
+    
+    def none_items_message(self):
+        actual_message = self.browser.find_element(*BasketPageLocators.NONE_PRODUCTS_MESSAGE).text
+        expected_message = "Your basket is empty"
+        assert expected_message in actual_message, f'Incorrect message, must be "{expected_message}"'
+        
+    @pytest.mark.xfail
+    def should_not_be_products_in_basket(self):
+        assert self.is_element_present(*BasketPageLocators.ITEMS_LINK), "Products in the basket, but shouldn't be there"
