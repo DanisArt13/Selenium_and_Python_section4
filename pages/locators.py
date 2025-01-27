6a16423a6bf7c54f3c4bca8d8d772097c8127197
Danis Art <nikita.efremov1412@gmail.com>
2025-01-27 10:46:32 +0300

    add LoginPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,33 @@
 """ locators """
 
 from selenium.webdriver.common.by import By
-
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    
+   
 class ButtonLocators():
     BUTTON_LINK = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    VIEW_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-group a.btn-default")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    # LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+    
+class BasketPageLocators():
+    ITEMS_LINK = (By.CSS_SELECTOR, "div.basket-items")
+    NONE_PRODUCTS_MESSAGE = (By.CSS_SELECTOR, "#content_inner p")
+
+class HomePageLocators():
+    HOME_PAGE_LINK = (By.CSS_SELECTOR ,".col-sm-7 a")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form") 
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form") 
+    LOGIN_EMAIL = (By.CSS_SELECTOR, 'input[name="login-username"]')
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, 'input[name="login-password"]')
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, 'input[name="registration-email"]')
+    REGISTRATION_PASSWORD1 = (By.CSS_SELECTOR, 'input[name="registration-password1"]')
+    REGISTRATION_PASSWORD2 = (By.CSS_SELECTOR, 'input[name="registration-password2"]')
+    REGISTRATION_SUBMIT = (By.CSS_SELECTOR, 'button[name="registration_submit"]')
+    
+class ProductPageLocators():
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alertinner")
+    
