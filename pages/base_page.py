86dcd6a1b0125d2a9f30f1350287135724cfc012
Danis Art <nikita.efremov1412@gmail.com>
2025-01-27 10:45:47 +0300

    add: def should_be_authorized_user, def should_be_login_link, def should_not_be_products_in_basket, def none_items_message, def go_to_login_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,10 @@
 """ base page """
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, NoAlertPresentException
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators, HomePageLocators, BasketPageLocators
+
 import math
 import time
 
@@ -11,9 +13,26 @@ class BasePage():
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+
+    def go_to_basket(self):
+        link = self.browser.find_element(*BasketPageLocators.ITEMS_LINK)
+        link.click()
         
-    def open(self):
-        self.browser.get(self.url)
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+    
+    def go_to_home_page(self):
+        link = self.browser.find_element(*HomePageLocators.HOME_PAGE_LINK)
+        link.click()
+        
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True 
         
     def is_element_present(self, how, what):
         try:
@@ -22,6 +41,21 @@ class BasePage():
             return False
         return True
         
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+        
+    def none_items_message(self):
+        actual_message = self.browser.find_element(*BasketPageLocators.NONE_PRODUCTS_MESSAGE).text
+        expected_message = "Your basket is empty"
+        assert expected_message in actual_message, f'Incorrect message, must be "{expected_message}"'
+        
+    def open(self):
+        self.browser.get(self.url)
+                
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
@@ -36,3 +70,15 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+    
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                 " probably unauthorised user"
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+        
+    def should_not_be_products_in_basket(self):
+        assert self.is_element_present(*BasketPageLocators.ITEMS_LINK), "Products in the basket, but shouldn't be there"
+        
+
+        
